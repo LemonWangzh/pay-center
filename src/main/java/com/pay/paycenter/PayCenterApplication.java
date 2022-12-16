46e6f76c75fe8e44a10216de194661a7e0e1af18
wangzh <wangzhenghao@abupdate.com>
2022-12-16 17:08:04 +0800

    Initial commit

---
+++ src/main/java/com/pay/paycenter/PayCenterApplication.java
@@ -0,0 +1,13 @@
+package com.pay.paycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayCenterApplication.class, args);
+    }
+
+}
